e20e35620f81ea975b88040b16ed8793f0715fce
nyCarvajal <nycarvajal@outlook.com>
2025-12-24 19:11:30 -0500

    Fix routing order for hospital board view

--- routes/web.php
+++ routes/web.php
@@ -217,7 +217,6 @@ Route::match(['GET','POST'], 'webhook/whatsapp',
 
 
  Route::get('', [RoutingController::class, 'index'])->name('root');
-    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
 
 // Vettech módulo V1
 Route::middleware(['auth'])
@@ -243,6 +242,7 @@ Route::middleware(['auth'])
         Route::post('cash/sessions/{cashSession}/close', [\App\Http\Controllers\CashSessionsController::class, 'close'])->name('cash.sessions.close');
         Route::post('cash/movements', [\App\Http\Controllers\CashMovementsController::class, 'store'])->name('cash.movements.store');
     });
+    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
     Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
     Route::get('{any}', [RoutingController::class, 'root'])->name('any');
 
