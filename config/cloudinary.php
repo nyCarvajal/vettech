75c49938611a28691fc0cbb46f631f7934ec8340
nyCarvajal <nycarvajal@outlook.com>
2026-01-08 10:00:36 -0500

    Add Cloudinary env fallbacks

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -4,8 +4,8 @@
 return [
     'cloud' => [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key'    => env('CLOUDINARY_API_KEY'),
-        'api_secret' => env('CLOUDINARY_API_SECRET'),
+        'api_key'    => env('CLOUDINARY_API_KEY', env('CLOUDINARY_KEY')),
+        'api_secret' => env('CLOUDINARY_API_SECRET', env('CLOUDINARY_SECRET')),
     ],
     'url' => [
         'secure' => true,
