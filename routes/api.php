2ceb632682a172faf5c5ba0ec2efec3e866673ce
nycarvajal <nycaravajal@outlook.com>
2026-03-18 12:11:52 -0500

    wehoob

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\WhatsAppWebhookController;
 
 Route::get('reserva/availability', [ReservaController::class, 'availability']);
 // routes/api.php  (o web.php si lo prefieres)
 Route::get('/clientesb', [ClientesController::class, 'search'])->name('clientes.search');
+
+
+Route::get('/webhook/whatsapp', [WhatsAppWebhookController::class, 'verify']);
+Route::post('/webhook/whatsapp', [WhatsAppWebhookController::class, 'receive']);
 
 
 ?>
